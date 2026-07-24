207c5a61a485510dc28233a5081898aa2d616d78
Logan <104459542+Mellocat@users.noreply.github.com>
2026-07-24 14:34:12 -0400

    jumpscare files, collecting system improvements, and more
    Added sounds when you collect, added the jumpscare video for a later placeholder, created a you died screen, and created a pause screen.

--- Assets/Scripts/CollectSystem.cs
+++ Assets/Scripts/CollectSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb8cbd2f0f34834f13de9e54e57e5b6fb255520fc2dc7c622762f0a5f0590464
-size 952
+oid sha256:337699c94af990b2e8f169a5be15ca629d194ba92d061657ae8d947fbfb7f2e4
+size 1610
