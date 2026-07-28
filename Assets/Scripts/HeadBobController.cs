95a92b7de81b8de1c357bd98559db83134d5e5b9
Nixsketch <nixsketch87@gmail.com>
2026-07-28 15:30:59 -0400

    lightmap and more coding

---
+++ Assets/Scripts/HeadBobController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68690d28c9c491d2e4eb6ef0b962be6061b4c671ea85866ea093fa23b74d1532
+size 2386
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0edec1a0835d129be7c110fe845bdced6ed132be0ddcef3da3053ae37c800e43
+oid sha256:eca64e6891f73f05a907f0fa9f5d9de61c8f5fd4561ddd15952526f24522b4ce
 size 2548
